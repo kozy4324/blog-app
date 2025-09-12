88c5e6d8c04677d8fc699901383073fdd5c321ae
Koji NAKAMURA <kozy4324@gmail.com>
2025-09-12 10:19:08 +0900

    test Article model

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "creation Article" do
+    article = Article.new(title: "My Test Article!!!")
+
+    assert article.save
+  end
+
+  test "title must be present" do
+    article = Article.new
+
+    refute article.save
+    assert_equal "Title can't be blank", article.errors.full_messages_for(:title).first
+  end
 end
