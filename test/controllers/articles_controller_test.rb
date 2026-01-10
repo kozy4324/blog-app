8848eb35132d149bed936e41719ae17964c6a715
Koji NAKAMURA <kozy4324@gmail.com>
2026-01-10 10:40:27 +0900

    Controller test

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,9 +1,11 @@
 require "test_helper"
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test "get index" do
     get articles_url
 
     assert_response :success
+    assert_match articles(:one).title, @response.body
+    assert_match articles(:two).title, @response.body
   end
 end
